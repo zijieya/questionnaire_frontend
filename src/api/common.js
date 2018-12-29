2e001d733dc8907c4a23ab8a80cd33a280a68403
zijieya <1347133477@qq.com>
2018-12-29 14:59:13 +0800

    update

---
+++ src/api/common.js
@@ -0,0 +1,61 @@
+import axios from '@/libs/api.request'
+
+export const activecode = ({ userserialid, verificationCode }) => {
+  const data = {
+    userserialid,
+    verificationCode
+  }
+  return axios.request({
+    url: 'activecode',
+    data,
+    method: 'post'
+  })
+}
+
+export const login = ({ password, username }) => {
+  const data = {
+    password,
+    username
+  }
+  return axios.request({
+    url: 'login',
+    data,
+    method: 'post'
+  })
+}
+
+export const password = ({ userserialid, password }) => {
+  const data = {
+    userserialid,
+    password
+  }
+  return axios.request({
+    url: 'password',
+    data,
+    method: 'put'
+  })
+}
+
+export const register = ({ email, username, password }) => {
+  const data = {
+    email,
+    username,
+    password
+  }
+  return axios.request({
+    url: 'register',
+    data,
+    method: 'post'
+  })
+}
+
+export const sendmail = ({ email }) => {
+  const data = {
+    email
+  }
+  return axios.request({
+    url: 'login',
+    data,
+    method: 'post'
+  })
+}
--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,54 @@
 import axios from '@/libs/api.request'
+export const generateServey = ({ creatorserialId, remark, tag, title, list }) => {
+  const data = {
+    creatorserialId,
+    remark,
+    tag,
+    title,
+    list
+  }
+  return axios.request({
+    url: '/user/generateServey',
+    data,
+    method: 'post'
+  })
+}
+export const serveylist = ({ keyword, pageIndex, pageSize }) => {
+  const data = {
+    keyword,
+    pageIndex,
+    pageSize
+  }
+  return axios.request({
+    url: 'user/servey',
+    data,
+    method: 'post'
+  })
+}
+export const serveydetail = ({ surveyserialid }) => {
+  const data = {
+    surveyserialid
+  }
+  return axios.request({
+    url: 'user/serveydetail',
+    data,
+    method: 'post'
+  })
+}
+export const submitservey = ({ questionid, surveyserialid, answererserialId, remark, submitType }) => {
+  const data = {
+    questionid,
+    surveyserialid,
+    answererserialId,
+    remark,
+    submitType
+  }
+  return axios.request({
+    url: 'user/submitServey',
+    data,
+    method: 'post'
+  })
+}
 
 export const login = ({ userName, password }) => {
   const data = {
